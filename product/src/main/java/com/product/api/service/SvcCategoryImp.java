860735240c98b6c8c3b5a04f2c1d1954fc759ab9
itzelfloresc <112104652+itzelfloresc@users.noreply.github.com>
2025-05-05 13:56:34 -0600

    Update product/src/main/java/com/product/api/service/SvcCategoryImp.java
    Co-authored-by: Mónica Miranda <53272126+monmm@users.noreply.github.com>

--- product/src/main/java/com/product/api/service/SvcCategoryImp.java
+++ product/src/main/java/com/product/api/service/SvcCategoryImp.java
@@ -117,7 +117,7 @@ public class SvcCategoryImp implements SvcCategory{
 			repo.updateCategory(id, in.getCategory(), in.getTag());
 			return new ResponseEntity<>(new ApiResponse("La categoría ha sido actualizada"), HttpStatus.OK);
 		}catch (DataAccessException e) {
-			if (e.getLocalizedMessage().contains("ux_region"))
+			if (e.getLocalizedMessage().contains("ux_category"))
 				throw new ApiException(HttpStatus.CONFLICT, "El nombre de la categoria ya está registrado");
 			if (e.getLocalizedMessage().contains("ux_tag"))
 				throw new ApiException(HttpStatus.CONFLICT, "El tag de la región ya está registrado");
